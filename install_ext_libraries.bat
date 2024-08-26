f0522fbda16b157285b7acf06eac4085a9ce1d49
MKTAARCOM <83835504+drphilllips@users.noreply.github.com>
2024-08-26 14:47:14 -0400

    Sort out app directory

---
+++ install_ext_libraries.bat
@@ -0,0 +1,10 @@
+pip install --user pyqt5==5.15.7
+pip install --user pandas==1.4.3
+pip install --user numpy==1.23.0
+pip install --user xlrd==2.0.1
+pip install --user openpyxl==3.0.10
+pip install --user xlsxwriter==3.0.3
+pip install --user pywin32==304
+pip install --user pywin32-ctypes==0.2.0
+pip install --user requests==2.31.0
+@pause
---
+++ uninstall (CAUTION)/uninstall_ext_libraries.bat
@@ -0,0 +1,10 @@
+pip uninstall pyqt5
+pip uninstall pandas
+pip uninstall numpy
+pip uninstall xlrd
+pip uninstall openpyxl
+pip uninstall xlsxwriter
+pip uninstall pywin32
+pip uninstall pywin32-ctypes
+pip uninstall requests
+@pause
